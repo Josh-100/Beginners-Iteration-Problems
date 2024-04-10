ce2f20de2bc6d255efb4e8e799c1f08a19b4f242
Josh-100 <160691712+Josh-100@users.noreply.github.com>
2024-04-10 08:08:21 +0000

    Realised problem stated it can't be negative so I added that condition as well

--- Multiplication.py
+++ Multiplication.py
@@ -1,4 +1,7 @@
-times_table_number = input("What number do you want its times table of? ")
+times_table_number = int(input("What number do you want its times table of? "))
+while (times_table_number < 0):
+    times_table_number = int(input("Number can't be negative. Enter a new number: "))
+
 for num in range(12):
-    times_number = (num+1) * int(times_table_number)
+    times_number = (num+1) * times_table_number
     print(str(times_number), end = " ")
